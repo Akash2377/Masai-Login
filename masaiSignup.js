cc371a04ab0339dbe25ec648eb7fe21492347833
Akash2377 <surveakash01@gmail.com>
2022-08-12 11:31:36 +0530

    Login and signup using local storage

---
+++ masaiSignup.js
@@ -0,0 +1,46 @@
+document.querySelector("form").addEventListener("submit", addUser);
+
+var userData = JSON.parse(localStorage.getItem("users")) || [];
+
+function addUser(e) {
+  e.preventDefault();
+  var userEmail1 = document.getElementById("userEmail").value;
+  var userMobile1 = document.getElementById("userMobile").value;
+  var userPassword1 = document.getElementById("userPassword").value;
+  if (userEmail1 != "" && userMobile1 != "" && userPassword1 != "") {
+    var user = {
+      email: userEmail1,
+      mobileNo: userMobile1,
+      password: userPassword1,
+    };
+
+    var ud = JSON.parse(localStorage.getItem("users")) || [];
+
+    var flag = ud.filter(function (el) {
+      return el.email == userEmail1;
+    });
+    if (flag.length >= 1) {
+      alert("User Already Sign Up");
+    } else {
+      userData.push(user);
+      localStorage.setItem("users", JSON.stringify(userData));
+      userEmail.value = "";
+      userMobile.value = "";
+      userPassword.value = "";
+      alert("Sign Up successfully");
+    }
+  } else {
+    alert("Please Fill All Information");
+  }
+}
+function myFunction() {
+  var x = document.getElementById("userPassword");
+  var y = document.getElementById("eye");
+  if (x.type === "password") {
+    x.type = "text";
+    y.src = "eye-solid.svg";
+  } else {
+    x.type = "password";
+    y.src = "eye-slash-solid.svg";
+  }
+}
---
+++ masailogin.js
@@ -0,0 +1,28 @@
+document.querySelector("form").addEventListener("submit", submitData);
+function submitData(e) {
+  e.preventDefault();
+  var userEmail1 = document.getElementById("userEmail").value;
+  var userPassword1 = document.getElementById("userPassword").value;
+  var userData = JSON.parse(localStorage.getItem("users"));
+  var flag = userData.filter(function (el) {
+    return el.email === userEmail1 && el.password === userPassword1;
+  });
+  if (flag.length >= 1) {
+    alert("Login Successfully");
+  } else {
+    alert("wrong credentials");
+  }
+  userEmail.value = "";
+  userPassword.value = "";
+}
+function myFunction() {
+  var x = document.getElementById("userPassword");
+  var y = document.getElementById("eye");
+  if (x.type === "password") {
+    x.type = "text";
+    y.src = "eye-solid.svg";
+  } else {
+    x.type = "password";
+    y.src = "eye-slash-solid.svg";
+  }
+}
